bf9b633d14a752bb92909e328a6f22cb4c5d4c24
Mikael <119380193+LifeIsCheese@users.noreply.github.com>
2025-04-26 18:22:32 -0600

    QOL, plot

--- out/html/new_shit.js
+++ out/html/new_shit.js
@@ -1,3 +1,13 @@
-function add () {
+function death () {
+    dendryUI.dendryEngine.state.qualities.bruning_plot = "successful";
+ }
+
+function fail () {
     dendryUI.dendryEngine.state.qualities.bruning_plot = "failed";
+ }
+function fifty () {
+    dendryUI.dendryEngine.state.qualities.bruning_plot = 50;
+ }
+function hundred () {
+    dendryUI.dendryEngine.state.qualities.bruning_plot = 100;
  }
