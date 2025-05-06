401f867e7ee3674ae8070d6d9c1f4efe83eed6ae
Mikael <119380193+LifeIsCheese@users.noreply.github.com>
2025-05-05 20:56:17 -0600

    give up

--- out/html/darkmode.js
+++ out/html/darkmode.js
@@ -4,9 +4,6 @@ const themeSwitch = document.getElementById('theme-switch')
 const enableDarkmode = () => {
   document.body.classList.add('darkmode')
   localStorage.setItem('darkmode', 'active')
-  window.dendryUI.disable_bg = true;
-document.body.style.backgroundImage = 'none';
-window.dendryUI.saveSettings();
 }
 
 const disableDarkmode = () => {
