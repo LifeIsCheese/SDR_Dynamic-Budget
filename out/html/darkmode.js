1d8f0f2ff62730b5fc84106637be35caa6b4343a
Mikael <119380193+LifeIsCheese@users.noreply.github.com>
2025-05-05 20:54:08 -0600

    maybe this

--- out/html/darkmode.js
+++ out/html/darkmode.js
@@ -4,6 +4,9 @@ const themeSwitch = document.getElementById('theme-switch')
 const enableDarkmode = () => {
   document.body.classList.add('darkmode')
   localStorage.setItem('darkmode', 'active')
+  window.dendryUI.disable_bg = true;
+document.body.style.backgroundImage = 'none';
+window.dendryUI.saveSettings();
 }
 
 const disableDarkmode = () => {
@@ -11,9 +14,7 @@ const disableDarkmode = () => {
   localStorage.setItem('darkmode', null)
 }
 
-if(darkmode === "active") enableDarkmode(); window.dendryUI.disable_bg = true;
-document.body.style.backgroundImage = 'none';
-window.dendryUI.saveSettings();
+if(darkmode === "active") enableDarkmode()
 
 themeSwitch.addEventListener("click", () => {
   darkmode = localStorage.getItem('darkmode')
