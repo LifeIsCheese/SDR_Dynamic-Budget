f80cb9d19e858247a5117eec21f81e502500f982
Mikael <119380193+LifeIsCheese@users.noreply.github.com>
2025-05-06 20:51:55 -0600

    453rewfds

--- out/html/new_shit.js
+++ out/html/new_shit.js
@@ -29,7 +29,7 @@ function hhundred () {
  function new_hire () {
    if (d.resources < 1) {
       d.director_pointer = Math.floor( Math.random() * d.director_a.length);
-      d.director_type = 1;
+      d.director_type = Math.floor( Math.random() * 2) + 1;
       d.director_s = d.director_a[d.director_pointer];
       d.resources -= 1;
       window.changeTab('status.Targets', 'Targets');
@@ -50,17 +50,13 @@ function director_actions () {
    window.changeTab('status.Actions', 'Actions')
 }
 function purge () {
-
-   if (d.loyalty_decay > 0) {
-      d.loyalty_decay -= 0.01;
-      d.interior_police_loyalty += 0.1;
-      d.prussian_police_loyalty += 0.1;
-      if (d.plot_target == "hjalmar") {
-         d.hjalmar_plot -= (d.plot_strength * 3);
-      }
-      if (d.plot_target == "bruning") {
-         d.bruning_plot -= (d.plot_strength * 3);
-      }
+   d.interior_police_loyalty += 0.1;
+   d.prussian_police_loyalty += 0.1;
+   if (d.plot_target == "hjalmar") {
+      d.hjalmar_plot -= (d.plot_strength * 3);
+   }
+   if (d.plot_target == "bruning") {
+      d.bruning_plot -= (d.plot_strength * 3);
    }
    d.director_actions_timer = 3;
    d.month_actions += 1;
@@ -71,8 +67,8 @@ function rush () {
    
    if (d.loyalty_decay < 0.04) {
       d.loyalty_decay += 0.01;
-      d.interior_police_loyalty -= 0.1;
-      d.prussian_police_loyalty -= 0.1;
+      d.interior_police_loyalty -= 0.05;
+      d.prussian_police_loyalty -= 0.05;
       if (d.plot_target == "hjalmar") {
          d.hjalmar_plot += (d.plot_strength * 3);
       }
@@ -84,4 +80,26 @@ function rush () {
    d.month_actions += 1;
    window.changeTab('status.Actions', 'Actions')
 
+}
+
+function train_spies () {
+   
+   if (d.loyalty_decay > 0) {
+      d.loyalty_decay -= 0.01;
+      }
+      if (d.spy_network < 9) {
+         d.spy_network += 3;
+      }
+      
+   d.director_actions_timer = 12;
+   d.month_actions += 1;
+   window.changeTab('status.Actions', 'Actions')
+}
+
+function train_police () {
+   d.prussian_police_strength += 10;
+   d.prussian_police_loyalty += 0.05;
+   d.director_actions_timer = 12;
+   d.month_actions += 1;
+   window.changeTab('status.Actions', 'Actions')
 }
