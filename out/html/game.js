c204adacc3beafc519296ec182c94f2b00c45f6c
LifeIsCheese <micoo0708@protonmail.com>
2025-06-10 23:19:03 -0600

    terf

--- out/html/game.js
+++ out/html/game.js
@@ -147,7 +147,7 @@
   };
 
 window.updateSidebarRight = function() {
-    $('#qualities_right').empty();
+    
     var scene = dendryUI.game.scenes[window.statusTabRight];
     dendryUI.dendryEngine._runActions(scene.onArrival);
     var displayContent = dendryUI.dendryEngine._makeDisplayContent(scene.content, true);
